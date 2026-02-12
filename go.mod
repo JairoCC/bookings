f86d9405b4fd9b87ef15fd81f4ae4d27953f2123
JairoCC <jairo.h.cubillos@gmail.com>
2026-02-12 07:40:43 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/JairoCC/bookings
+
+go 1.22.3
+
+require (
+	github.com/alexedwards/scs/v2 v2.8.0
+	github.com/go-chi/chi/v5 v5.2.5
+	github.com/justinas/nosurf v1.2.0
+)
---
+++ pkg/handlers/render/render.go
@@ -0,0 +1,93 @@
+package render
+
+import (
+	"bytes"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+
+	"github.com/JairoCC/bookings/pkg/config"
+	"github.com/JairoCC/bookings/pkg/models"
+)
+
+var app *config.AppConfig
+
+// NewTemplates sets the config for the template package
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
+
+func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+	return td
+}
+
+// RenderTemplate using html template
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+
+	var tc map[string]*template.Template
+	if app.UseCache {
+		// get template cache from the AppConfig
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
+	}
+
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal(ok)
+	}
+
+	buf := new(bytes.Buffer)
+	err := t.Execute(buf, td)
+
+	td = AddDefaultData(td)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	// render the template
+	_, err = buf.WriteTo(w)
+
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+
+	//get all the files named *.page.html from ./templates
+
+	pages, err := filepath.Glob("./templates/*.page.html")
+
+	if err != nil {
+		return myCache, err
+	}
+
+	// range through all files ending with *.page.html
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).ParseFiles(page)
+
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.html")
+
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.html")
+		}
+
+		myCache[name] = ts
+	}
+
+	return myCache, nil
+}
